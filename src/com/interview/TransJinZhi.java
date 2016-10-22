1cb9d9560fdfbc9e8fa9b2c4c119c8326aedf943
peipei1109 <369758824@qq.com>
2016-10-22 14:23:29 +0800

    修正进制转换

--- src/com/interview/TransJinZhi.java
+++ src/com/interview/TransJinZhi.java
@@ -2,15 +2,25 @@ package com.interview;
 
 import java.awt.print.Printable;
 
-public class TransJinZhi 
-{
-    public static void main(String[] args) 
+public class TransJinZhi{
+	public static void main(String[] args) 
     {
-        toHex(60);
-        toba(60);
-        toBin(60);
-        trans (17,17,5);
+		
+		TenTo2x trans1 =new TenTo2x();
+		trans1.toHex(16);
+		trans1.toba(60);
+		trans1.toBin(60);
+		
+		new TenTo36().trans (18,17);
     }
+}
+
+
+
+class TenTo2x
+{
+	//有bug，只能转换成进制数为2幂次方的进制
+    
 
 //十进制转二进制
     public static void toBin (int num)
@@ -54,11 +64,9 @@ public class TransJinZhi
         {
 //            得出指定进制中每一位的数值，存入一个数组中。
             int temp = num & base;
-            System.out.println(temp);
             arr[--pos] = chs[temp];
 //            每得出一个指定进制一个位上的值后，原数值的二进制位就往右移动指定位数，并在前面补零。
             num = num >>> offset;      //移位补零。
-            System.out.println(num);
         }
         
         for(int x=pos;x<arr.length;x++)
@@ -70,4 +78,50 @@ public class TransJinZhi
             }
         }
     }
+}
+
+
+class TenTo36{
+	
+	
+    public static void trans (int num,int base)
+    {
+//        定义一个数组，角标对应数值，可以看做一个查询表。
+        char[] chs = {'0' , '1' , '2' , '3' , '4' , '5' ,
+        		  '6' , '7' , '8' , '9' , 'a' , 'b' ,
+        		  'c' , 'd' , 'e' , 'f' , 'g' , 'h' ,
+        		  'i' , 'j' , 'k' , 'l' , 'm' , 'n' ,
+        		  'o' , 'p' , 'q' , 'r' , 's' , 't' ,
+        		  'u' , 'v' , 'w' , 'x' , 'y' , 'z'};
+        char[] arr = new char[32];
+        int pos = arr.length;
+        int rest=0;
+
+        if(num==0)
+        {
+//            如果要转换的数为0，则无需转换。
+            System.out.println(0);
+        }
+
+        while(num!=0)
+        {
+//            得出指定进制中每一位的数值，存入一个数组中。
+            
+            rest=num%base;
+            num=num/base;
+            
+            arr[--pos] = chs[rest];
+//            System.out.println("余数："+num+','+rest+','+arr[--pos]);
+        }
+        
+        for(int x=pos;x<arr.length;x++)
+        {
+            if (arr.length-1==x) {
+                System.out.println(arr[x]);                
+            } else {
+                System.out.print(arr[x]);    
+            }
+        }
+    }
+	
 }
